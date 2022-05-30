9920dffdec232fbfa2b98100646e785116e10230
AIN086 <novozhilov952@gmail.com>
2022-05-30 20:01:00 +0500

    Init commit

---
+++ src/main/java/Person.java
@@ -0,0 +1,80 @@
+import java.util.Objects;
+
+public class Person {
+
+    private final String name;
+    private final String surname;
+    private int age;
+    private String address;
+
+    public Person(String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+    }
+
+    public Person(String name, String surname, int age){
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+    }
+
+    public boolean hasAge(){
+        return this.age >= 0;
+    }
+
+    public void happyBirthday(){
+        if(this.hasAge()){
+            age++;
+        }
+    }
+
+    public boolean hasAddress(){
+        return this.address != null;
+    }
+
+    public void setAddress(String address){
+        this.address = address;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public PersonBuilder newChildBuilder(){
+        PersonBuilder personBuilder = new PersonBuilder();
+        personBuilder.setSurname(this.surname);
+        personBuilder.setAddress(this.address);
+        return personBuilder;
+    }
+
+    @Override
+    public String toString(){
+        return name + " " + surname + " (" + (hasAge() ? age : "") + (hasAddress() ? address : "") + ")";
+    }
+
+    @Override
+    public boolean equals(Object o){
+        if(this == o) return true;
+        if(!(o instanceof Person person )) return false;
+        return getAge() == person.getAge() && Objects.equals(getName(), person.getName())
+                && Objects.equals(getSurname(), person.getSurname())
+                && Objects.equals(getAddress(), person.getAddress());
+    }
+
+    @Override
+    public int hashCode(){
+        return Objects.hash(getName(), getSurname(),getAge(),getAddress());
+    }
+}
